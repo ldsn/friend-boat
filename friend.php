fb0fbe6737ca8fcc7c4e9a909f379952194cf8a0
arthurjason <wangfengxin@ldustu.com>
2016-04-13 00:59:05 +0800

    完成

--- friend.php
+++ friend.php
@@ -5,9 +5,9 @@ $im = imagecreatefromjpeg("./friend.jpg");
 
 
 imagealphablending($im, true);
-$text1 = '你是哪个公司的？';
-$text2 = '爱奇艺';
-$text3 = '送我一年会员吧';
+$text1 = $_GET['text1'] ? $_GET['text1'] : '第一句话';
+$text2 = $_GET['text2'] ? $_GET['text2'] : '第二句话';
+$text3 = $_GET['text3'] ? $_GET['text3'] : '第三句话';
 
 imagefttext($im, 14, 0, 70, 30, $red, 'SIMFANG.TTF', $text1);
 imagefttext($im, 14, 0, 230, 60, $red, 'SIMFANG.TTF', $text2);
